69ab80e12d7ae9929d616a8a59ef8005bb378361
Luisa López <lmlopez.19@est.ucab.edu.ve>
2022-11-04 22:50:14 -0400

    Fix favorites

--- src/pages/Favoritos.tsx
+++ src/pages/Favoritos.tsx
@@ -1,17 +1,6 @@
 import { SpeedDial } from "@mui/lab";
-import { 
-  Box, 
-  Container, 
-  SpeedDialAction, 
-  SpeedDialIcon, 
-  Typography, 
-  Tab,
-} from "@mui/material";
-import {
-  TabContext,
-  TabList,
-  TabPanel,
-} from "@mui/lab";
+import { Box, Container, SpeedDialAction, SpeedDialIcon, Typography, Tab } from "@mui/material";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
 import Fade from "@mui/material/Fade";
 import React, { useState } from "react";
 import { NavBar } from "../components/NavBar";
@@ -21,10 +10,12 @@ import {
   HailRounded as PasajeroFavoritoIcon,
   DriveEtaRounded as ConductorFavoritoIcon,
 } from "@mui/icons-material";
+import ListaUsuarios from "../components/ListaUsuarios";
 
 const Favoritos = (): JSX.Element => {
   const [isDialogPasajeroFavoritoOpen, setDialogPasajeroFavoritoOpen] = useState(false);
   const [isDialogConductorFavoritoOpen, setDialogConductorFavoritoOpen] = useState(false);
+  const [value, setValue] = React.useState("1");
   const openPasajeroFavoritoDialog = () => {
     setDialogPasajeroFavoritoOpen(true);
   };
@@ -48,23 +39,21 @@ const Favoritos = (): JSX.Element => {
     <Box>
       <NavBar />
       <TabContext value={value}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
           <TabList onChange={handleChange} variant="fullWidth">
             <Tab label="Conductores" value="1" />
             <Tab label="Pasajeros" value="2" />
           </TabList>
         </Box>
         <TabPanel value="1">
-          <ListaUsuarios/>
+          <ListaUsuarios />
         </TabPanel>
         <TabPanel value="2">
-          <ListaUsuarios/>
+          <ListaUsuarios />
         </TabPanel>
       </TabContext>
       <Fade in timeout={800}>
         <Box>
-          
-
           <Container maxWidth="md" sx={{ p: 3 }}>
             {/* <Typography
               color="primary"
@@ -75,8 +64,6 @@ const Favoritos = (): JSX.Element => {
             >
               Favoritos
             </Typography>{" "} */}
-
-            
 
             <SpeedDial
               ariaLabel="acciones"
