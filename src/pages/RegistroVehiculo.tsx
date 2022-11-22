edbb57429c997445347858ac0ca5be490a3923eb
Maria Guerra <mariafgg53@gmail.com>
2022-11-21 21:40:51 -0400

    Fix vehicle registrer
    ahorita fue q me acorde de subirlo perd0n

--- src/pages/RegistroVehiculo.tsx
+++ src/pages/RegistroVehiculo.tsx
@@ -2,6 +2,7 @@ import {
   Box,
   Button,
   ButtonGroup,
+  Card,
   Container,
   Divider,
   Fade,
@@ -29,7 +30,7 @@ import Link from "../components/Link";
 import { useId } from "react";
 import Select from "../components/Select";
 import SubPaginasHeader from "../components/SubPaginasHeader";
-import logo from "../assets/logo512.png";
+import car from "../assets/car.png";
 
 interface Values {
   placa: string;
@@ -73,53 +74,43 @@ const RegistroVehiculo = (): JSX.Element => {
       <SubPaginasHeader pageName="Registro de Vehículo" />
       <Fade in timeout={800}>
         <Box
-          maxWidth="md"
+          //maxWidth="md"
+          m={-1}
           sx={{
-            p: 3,
             display: "flex",
             flexDirection: "column",
+            justifyContent: "center",
             alignContent: "center",
           }}
         >
-          <Box component="img" src={logo} alt="logo" sx={{ width: "70%", mb: 1 }} />
-          <Formik initialValues={initialValues} validationSchema={schema} onSubmit={onSubmit}>
-            {({ isSubmitting }) => (
-              <Stack component={Form} spacing={2} justifyContent="center" alignContent={"center"}>
-                <Field component={TextField} name="placa" label="Placa del vehículo" required />
-                <Field component={TextField} name="color" label="Color del vehículo" required />
-                <Field component={TextField} name="modelo" label="Modelo del vehículo" required />
+          <Container 
+            maxWidth={false} 
+            sx={{ 
+              maxWidth: 450,
+              mb: 3, 
+              mt: 12,
+              position: "relative",
+            }}
+          >
+            <Formik initialValues={initialValues} validationSchema={schema} onSubmit={onSubmit}>
+              {({ isSubmitting }) => (
+                <Stack component={Form} spacing={2} justifyContent="center" alignContent={"center"}>
+                  <Field component={TextField} name="placa" label="Placa del vehículo" required />
+                  <Field component={TextField} name="color" label="Color del vehículo" required />
+                  <Field component={TextField} name="modelo" label="Modelo del vehículo" required />
 
-                <LoadingButton type="submit" loading={isSubmitting} variant="contained">
-                  Registrar vehículo
-                </LoadingButton>
-              </Stack>
-            )}
-          </Formik>
+                  <LoadingButton type="submit" loading={isSubmitting} variant="contained">
+                    Registrar vehículo
+                  </LoadingButton>
+                </Stack>
+              )}
+            </Formik>
+          </Container>
         </Box>
       </Fade>
+      <Box component="img" src={car} alt="car" sx={{ width: 400, position: "absolute", bottom: 0, right: 0, }} />
     </Box>
   );
-  // return (
-  //   <Box>
-  //     <SubPaginasHeader pageName="Registro de vehículo" />
-  //     <Fade in timeout={800}>
-  //       <Box>
-  //         <Container maxWidth="md" sx={{ p: 3, mt: 4 }}>
-  //           <Typography
-  //             color="primary"
-  //             textAlign="center"
-  //             fontSize={{ xs: 27, md: 30 }}
-  //             fontWeight={600}
-  //             mb={{ xs: 2, sm: 3 }}
-  //           >
-  //             Registro de vehículo
-  //           </Typography>
-
-  //         </Container>
-  //       </Box>
-  //     </Fade>
-  //   </Box>
-  // );
 };
 
 export default RegistroVehiculo;
