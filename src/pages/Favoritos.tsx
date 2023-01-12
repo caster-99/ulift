0a32b0c6450708d09b9e84ad7493e7f9f8131d3a
Maria Guerra <mariafgg53@gmail.com>
2023-01-12 17:50:13 -0400

    Fix favorites

--- src/pages/Favoritos.tsx
+++ src/pages/Favoritos.tsx
@@ -1,5 +1,5 @@
 import { SpeedDial } from "@mui/lab";
-import { Box, Container, SpeedDialAction, SpeedDialIcon, Typography, Tab } from "@mui/material";
+import { Box, Container, SpeedDialAction, SpeedDialIcon, Typography, Fab } from "@mui/material";
 import { TabContext, TabList, TabPanel } from "@mui/lab";
 import Fade from "@mui/material/Fade";
 import React, { useEffect, useState } from "react";
@@ -9,15 +9,16 @@ import PasajeroFavoritoDialogo from "../components/PasajeroFavoritoDialogo";
 import {
   HailRounded as PasajeroFavoritoIcon,
   DriveEtaRounded as ConductorFavoritoIcon,
+  Add as AgregarUsuarioIcon,
 } from "@mui/icons-material";
 import ListaUsuarios from "../components/ListaPasajeros";
 import ListaConductores from "../components/ListaConductores";
 import axios from "axios";
 import { User } from "../types";
 
-const Favoritos = (): JSX.Element => {
-  var favoritos: User[] = [];
+var favoritos: User[] = [];
 
+const Favoritos = (): JSX.Element => {
   const fetchFav = () => {
     const token = localStorage.getItem("token");
 
@@ -30,8 +31,19 @@ const Favoritos = (): JSX.Element => {
     axios(config)
       .then(function (response) {
         console.log(JSON.stringify(response.data.favorites));
-        favoritos = response.data.favorites;
+        favoritos = response.data.favorites.map((fav: any) => {
+          var usuarios = {} as User; //arreglo auxiliar
+
+          usuarios.name = fav.nameU;
+          usuarios.trips = fav.n_trips;
+          usuarios.rating = fav.rate;
+          usuarios.photo = fav.photo;
+
+          return usuarios;
+        });
+
         console.log(favoritos);
+        console.log(favoritos.length);
       })
       .catch(function (error) {
         console.log(error);
@@ -39,7 +51,6 @@ const Favoritos = (): JSX.Element => {
   };
 
   const [isDialogPasajeroFavoritoOpen, setDialogPasajeroFavoritoOpen] = useState(false);
-  const [isDialogConductorFavoritoOpen, setDialogConductorFavoritoOpen] = useState(false);
   const [value, setValue] = React.useState("1");
   const openPasajeroFavoritoDialog = () => {
     setDialogPasajeroFavoritoOpen(true);
@@ -49,95 +60,13 @@ const Favoritos = (): JSX.Element => {
     setDialogPasajeroFavoritoOpen(false);
   };
 
-  const openConductorFavoritoDialog = () => {
-    setDialogConductorFavoritoOpen(true);
-  };
-
-  const closeConductorFavoritoDialog = () => {
-    setDialogConductorFavoritoOpen(false);
-  };
-
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
 
   useEffect(() => {
     fetchFav();
-  }, []);
-
-  const verTipo = () => {
-    //Aqui tienes que agarrar el arreglo de favoritos y ver el tipo de favoritos que hay segun lo que esta en la BD
-  };
-
-  const p1: User = {
-    name: "Luisa",
-    email: "luisa",
-    gender: "Femenino",
-    role: "Estudiante",
-    id: "1234",
-    photo: "https://i.imgur.com/0cQ3X7A.png",
-    trips: 0,
-    rating: 0,
-    emergencyContact: "123456789",
-    emergencyName: "Luisa",
-    vehicles: [],
-    destinations: [],
-    routes: [],
-  };
-
-  const p2: User = {
-    name: "Maria",
-    email: "luisa",
-    gender: "Femenino",
-    role: "Estudiante",
-    id: "11121",
-    photo: "https://i.imgur.com/0cQ3X7A.png",
-    trips: 0,
-    rating: 0,
-    emergencyContact: "123456789",
-    emergencyName: "Luisa",
-    vehicles: [],
-    destinations: [],
-    routes: [],
-  };
-
-  const p3: User = {
-    name: "Ana",
-    email: "luisa",
-    gender: "Femenino",
-    role: "Estudiante",
-    id: "4567",
-    photo: "https://i.imgur.com/0cQ3X7A.png",
-    trips: 0,
-    rating: 0,
-    emergencyContact: "123456789",
-    emergencyName: "Luisa",
-    vehicles: [],
-    destinations: [],
-    routes: [],
-  };
-
-  const p4: User = {
-    name: "Eva",
-    email: "luisa",
-    gender: "Femenino",
-    role: "Estudiante",
-    id: "8910",
-    photo: "https://i.imgur.com/0cQ3X7A.png",
-    trips: 0,
-    rating: 0,
-    emergencyContact: "123456789",
-    emergencyName: "Luisa",
-    vehicles: [],
-    destinations: [],
-    routes: [],
-  };
-  var pasajeros: User[] = [];
-
-  pasajeros.push(p1);
-  pasajeros.push(p2);
-  pasajeros.push(p3);
-  pasajeros.push(p4);
+  });
 
   return (
     <Box>
@@ -155,60 +84,31 @@ const Favoritos = (): JSX.Element => {
             >
               Favoritos
             </Typography>
-            <TabContext value={value}>
-              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-                <TabList onChange={handleChange} variant="fullWidth">
-                  <Tab label="Conductores" value="1" />
-                  <Tab label="Pasajeros" value="2" />
-                </TabList>
-              </Box>
-              <TabPanel value="1">
-                {favoritos.length === 0 && (
-                  <Typography fontSize={{ xs: 14, md: 17 }}>
-                    Aun no tienes conductores favoritos
-                  </Typography>
-                )}
-                {favoritos.length! > 0 && <ListaConductores conductores={pasajeros} />}
-              </TabPanel>
-              <TabPanel value="2">
-                {favoritos.length === 0 && (
-                  <Typography fontSize={{ xs: 14, md: 17 }}>
-                    Aun no tienes pasajeros favoritos
-                  </Typography>
-                )}
-                {favoritos.length! > 0 && <ListaUsuarios pasajeros={pasajeros} />}
-              </TabPanel>
-            </TabContext>
-            <SpeedDial
-              ariaLabel="acciones"
-              sx={{ position: "fixed", bottom: "8%", right: "5%" }}
-              icon={<SpeedDialIcon />}
-              FabProps={{
-                color: "secondary",
-                size: "large",
+
+            {favoritos.length === 0 && (
+              <Typography fontSize={{ xs: 14, md: 17 }}>
+                Aun no tienes usuarios favoritos
+              </Typography>
+            )}
+            {favoritos.length! > 0 && <ListaUsuarios pasajeros={favoritos} />}
+
+            <Fab
+              aria-label="agregar"
+              color="secondary"
+              size="large"
+              sx={{
+                position: "fixed",
+                bottom: "8%",
+                right: "5%",
               }}
+              onClick={openPasajeroFavoritoDialog}
             >
-              <SpeedDialAction
-                icon={<ConductorFavoritoIcon />}
-                onClick={openConductorFavoritoDialog}
-                tooltipTitle="Conductor"
-                tooltipOpen
-              />
-              <ConductoresFavoritosDialogo
-                isOpen={isDialogConductorFavoritoOpen}
-                closeDialog={closeConductorFavoritoDialog}
-              />
-              <SpeedDialAction
-                icon={<PasajeroFavoritoIcon />}
-                onClick={openPasajeroFavoritoDialog}
-                tooltipTitle="Pasajero"
-                tooltipOpen
-              />
-              <PasajeroFavoritoDialogo
-                isOpen={isDialogPasajeroFavoritoOpen}
-                closeDialog={closePasajeroFavoritoDialog}
-              />
-            </SpeedDial>
+              <AgregarUsuarioIcon />
+            </Fab>
+            <PasajeroFavoritoDialogo
+              isOpen={isDialogPasajeroFavoritoOpen}
+              closeDialog={closePasajeroFavoritoDialog}
+            />
           </Container>
         </Box>
       </Fade>
