4090d089f29576789cc9b4646c7ff33ac480607d
Maria Guerra <mariafgg53@gmail.com>
2023-01-13 11:25:23 -0400

    Chatsito

---
+++ src/components/ChatBubble.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  ListItem,
+  Avatar,
+  ListItemAvatar,
+  ListItemText,
+  IconButton,
+  Stack,
+  Box,
+  Typography,
+  Card,
+  CardActionArea,
+  Grid,
+} from "@mui/material";
+import {} from "@mui/icons-material";
+//import ChatBubble from "react-chat-bubble";
+
+interface UserProps {
+  message: string;
+}
+
+const ChatBubble = (props: UserProps): JSX.Element => {
+  return <Box></Box>;
+};
+
+export default ChatBubble;
